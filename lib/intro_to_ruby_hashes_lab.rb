5c90b29136bbb2f214e9dd37677129e1a62f0a99
Carly L <carlylynnla@gmail.com>
2020-05-05 02:00:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] == nil
-    {key => 1}
+  if hash[key]
+    hash[key] = hash[key] + 1
   else
-  {key => hash[key] + 1}
-end
-hash
+  hash[key] = 1
+  end
 end
